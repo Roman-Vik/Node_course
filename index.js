9669892911d8a9b1decbb2c31c6b67249baf54fe
Roman Simonov <roman.viktorovich@outlook.com>
2023-02-08 16:41:46 +0300

    work with express.Router

--- index.js
+++ index.js
@@ -5,21 +5,15 @@ const PORT = 7000
 const HOST = 'localhost'
 const app = e()
 
-//путь, обработчик,,
-//next - функция промежуточной обработки
 
-
+/*
 const prod = ['apple', 'pen', 'comp']
-
 app.get('/', (req, res, next)=>{
     res.status(200).send(new Date())
 })
-
 app.get('/products', (req, res)=>{
     res.json(prod)
 })
-
-
 app.get('/products/:id', (req, res, next)=>{
     console.log('page products:', req.query.t)
     if(prod[req.params.id]){
@@ -28,6 +22,21 @@ app.get('/products/:id', (req, res, next)=>{
         res.status(403).send("product not found")
     }
 })
+*/
+booksRouter.get('/', (req, res)=> {
+res.send('Books')
+})
+booksRouter.get('/about', (req, res)=> {
+    res.send('About books')
+})
+
+
+app.use('/books', booksRouter)
+
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`http://${HOST}:${PORT}/`, new Date())
